16a7245f8a5eae22010d487df68c33f6b053ad66
Sinkholy <sinkholy@gmail.com>
2023-03-04 12:54:34 +0300

    [Dev] Начало приведения UI к приемлемому виду.

--- Assets/UI/Scripts/IngameUIScript.cs
+++ Assets/UI/Scripts/IngameUIScript.cs
@@ -1,10 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 using Assets.Scripts.Entities;
+using Assets.Scripts.Movement.Behaviour;
+
+using Unity.VisualScripting;
 
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
+
+using static DefaultActions;
 
 public class IngameUIScript : MonoBehaviour
 {
@@ -12,30 +19,107 @@ public class IngameUIScript : MonoBehaviour
     [SerializeField]
     Player player;
 
-    Label velocityLabel;
-    Label coordinatesLabel;
-    Label directionLabel;
+    Input input;
+
+    Label laserChargerText;
+    ProgressBar laserCooldownBar;
+
+    Label rotationText;
+
+    Label coordinatesText;
+
+    ProgressBar accelerationBar;
+
+    ProgressBar velocityBar;
 
     // Start is called before the first frame update
     void Start()
     {
+        input = new Input();
         uiDoc = GetComponent<UIDocument>();
 
         var root = uiDoc.rootVisualElement;
 
-        var labels = uiDoc.rootVisualElement.Q<Label>();
+        laserCooldownBar = root.Q<ProgressBar>("LaserCooldownBar");
+        laserChargerText = root.Q<Label>("LaserChargesText");
 
-        velocityLabel = uiDoc.rootVisualElement.Q<Label>("Velocity");
-        coordinatesLabel = uiDoc.rootVisualElement.Q<Label>("Coordinates");
-        directionLabel = uiDoc.rootVisualElement.Q<Label>("Rotation");
-    }
+        rotationText = root.Q<Label>("RotationText");
+
+        coordinatesText = root.Q<Label>("CoordinatesText");
+
+        accelerationBar = root.Q<ProgressBar>("AccelerationBar");
+
+        velocityBar = root.Q<ProgressBar>("VelocityBar");
+	}
 
     // Update is called once per frame
     void Update()
     {
-        velocityLabel.text = player.Velocity.ToString();
-		directionLabel.text = player.RotationAngle.ToString();
-		coordinatesLabel.text = player.WorldCoordinates.ToString();
-        
+        accelerationBar.value = input.AccelerationInput;
+        UpdateVelocityUI();
+        UpdateLaserUI();
+        rotationText.text = Math.Round(player.RotationAngle).ToString();
+        coordinatesText.text = player.WorldCoordinates.ToString();
+
+        void UpdateVelocityUI()
+        {
+			velocityBar.value = GetVelocityBarValue();
+			velocityBar.title = $"Velocity: {Math.Round(player.Velocity, 1)}/{player.GetComponent<PlayerControllableMovementBehaviour>().MaxVelocity}";
+
+
+			float GetVelocityBarValue()
+			{
+				float maxVelocity = player.GetComponent<PlayerControllableMovementBehaviour>().MaxVelocity;
+				float percent = 100 / maxVelocity;
+
+				return Mathf.Round(player.Velocity * percent);
+			}
+		}
+        void UpdateLaserUI()
+        {
+
+			laserCooldownBar.value = GetLaserCooldown();
+			laserChargerText.text = GetLaserChargesFormated();
+			laserCooldownBar.title = $"Charge: {GetLaserCooldown()}%";
+
+
+			string GetLaserChargesFormated()
+			{
+				return $"{player.LaserCharges}/{5}";
+			}
+			float GetLaserCooldown()
+			{
+				const float ChargeCooldown = 5;
+				float percent = 100 / ChargeCooldown;
+
+				return Mathf.Round((ChargeCooldown - player.LaserChargeCooldown) * percent);
+			}
+		}
+    }
+
+	class Input : IMovementActions
+	{
+		internal float AccelerationInput { get; private set; }
+
+		DefaultActions actions;
+
+		internal Input()
+		{
+			actions = new DefaultActions();
+			actions.Movement.SetCallbacks(this);
+            if (!actions.Movement.enabled)
+            {
+				actions.Movement.Enable();
+			}
+		}
+
+		public void OnAccelerate(InputAction.CallbackContext context)
+		{
+			AccelerationInput = context.action.ReadValue<float>();
+		}
+
+        public void OnRotate(InputAction.CallbackContext context)
+        {
+        }
     }
 }
