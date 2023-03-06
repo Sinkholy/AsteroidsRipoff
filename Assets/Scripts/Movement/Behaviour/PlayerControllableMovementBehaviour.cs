5870759505fe7ddab2858ef37e19f3bbb7d099e7
Sinkholy <sinkholy@gmail.com>
2023-03-07 00:10:53 +0300

    [Dev] Добавил возможность извне получить информацию о максимальной скорости.

--- Assets/Scripts/Movement/Behaviour/PlayerControllableMovementBehaviour.cs
+++ Assets/Scripts/Movement/Behaviour/PlayerControllableMovementBehaviour.cs
@@ -20,6 +20,8 @@ namespace Assets.Scripts.Movement.Behaviour
 		MovementInputReader input;
 		DynamicBodyMovementController movementController;
 
+		internal float MaxVelocity => maxSpeed;
+
 		#region Unity callbacks
 		void Awake()
 		{
