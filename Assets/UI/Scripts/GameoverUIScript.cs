8b46c9b97e0a555904296d463eda9c82c3563aa0
Sinkholy <sinkholy@gmail.com>
2023-03-06 21:09:30 +0300

    [Dev] Добавил кнопку выхода в окно окончания игры.

--- Assets/UI/Scripts/GameoverUIScript.cs
+++ Assets/UI/Scripts/GameoverUIScript.cs
@@ -19,12 +19,27 @@ namespace Assets.UI.Scripts
 				GameManager.LoadGame();
 			};
 
+			uiDoc.rootVisualElement.Q<Button>("ExitButton").clicked += () =>
+			{
+				Quit();
+			};
+
 			uiDoc.rootVisualElement.Q<Label>("ScoreText").text = Overseer.LastSessionPlayerScore.ToString();
 		}
 
 		void Awake()
 		{
 			uiDoc = GetComponent<UIDocument>();
+		}
+
+		void Quit()
+		{
+			#if UNITY_STANDALONE
+			Application.Quit();
+			#endif
+			#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+			#endif
 		}
 	}
 }
