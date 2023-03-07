2489b34fc02cbc5291355197a714a4ef3f1cc4f1
Sinkholy <sinkholy@gmail.com>
2023-03-07 18:49:16 +0300

    [Dev] Добавил селектор сложности на стартовый экран.

--- Assets/UI/Scripts/InitialUI.cs
+++ Assets/UI/Scripts/InitialUI.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 using Assets.Scripts;
 
 using UnityEngine;
@@ -9,17 +11,37 @@ namespace Assets.UI.Scripts
 	public class InitialUI : MonoBehaviour
 	{
 		UIDocument uiDoc;
+		DropdownField difficultyDDL;
 
 		void Awake()
 		{
-			uiDoc = GetComponent <UIDocument>();
+			uiDoc = GetComponent<UIDocument>();
 		}
 		void Start()
 		{
-			uiDoc.rootVisualElement.Q<Button>("PlayButton").clicked += () =>
+			uiDoc.rootVisualElement.Q<Button>("PlayButton").clicked += OnPlayButtonClicked;
+
+			difficultyDDL = uiDoc.rootVisualElement.Q<DropdownField>("DifficultyDDL");
+			difficultyDDL.choices = GameManager.AvailableDifficulties.Select(x => x.Name).ToList();
+			difficultyDDL.index = 0;
+		}
+
+		void OnPlayButtonClicked()
+		{
+			SetDifficulty();
+			LoadGame();
+
+
+			void SetDifficulty()
+			{
+				var chosenDifficultyIndex = difficultyDDL.index;
+				var chosenDifficulty = GameManager.AvailableDifficulties[chosenDifficultyIndex];
+				GameManager.Difficulty = chosenDifficulty;
+			}
+			void LoadGame()
 			{
 				GameManager.LoadGame();
-			};
+			}
 		}
 	}
 }
